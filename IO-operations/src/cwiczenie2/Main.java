ab0898831513d3ae42ba62cf559db8ab3bc0b1fa
Szymon Gąsior <szgasior@outlook.com>
2018-02-05 12:17:41 +0100

    Implementation of example 2

---
+++ IO-operations/src/cwiczenie2/Main.java
@@ -0,0 +1,18 @@
+package cwiczenie2;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		String input;
+		
+		while(!(input = scanner.nextLine()).equals("exit")) {
+			System.out.println("Wpisales: " + input);
+		}
+		
+		scanner.close();
+	}
+
+}
